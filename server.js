1863b40e1a56deb08f6e94c9dd685f96adad15cb
IIInvokeII <nintendobadri@gmail.com>
2021-10-30 19:32:47 +0530

    expenditure now more detailed

--- server.js
+++ server.js
@@ -52,20 +52,38 @@ app.use(passport.session())
 app.use(methodOverride('_method'))
 
 
-app.get('/', checkAuthenticated,  (req, res) => {    
-  res.render('dashboard.ejs', { name: req.user.name, admin: req.user.admin, apartment: req.user.apartment })
+app.get('/',checkAuthenticated, (req,res) => {
+  res.render('expenditure.ejs', { 
+    id: req.user._id, 
+    secret: process.env.API_SECRET, 
+    name: req.user.name, 
+    admin: req.user.admin, 
+    apartment: req.user.apartment, 
+    doorNumber: req.user.doorNumber,
+    contact: req.user.contact
+   })
 })
 
 app.get('/new_expense', checkAuthenticated,  (req, res) => {    
-  res.render('expenses.ejs', { name: req.user.name, admin: req.user.admin, apartment: req.user.apartment })
+  res.render('expenses.ejs', { 
+    name: req.user.name, 
+    secret: process.env.API_SECRET, 
+    admin: req.user.admin, 
+    apartment: req.user.apartment,
+    doorNumber: req.user.doorNumber,
+    contact: req.user.contact
+  })
 })
 
 app.get('/water_reading', checkAuthenticated, (req,res) => {
-  res.render('water_reading.ejs',{ name: req.user.name, admin: req.user.admin, apartment: req.user.apartment })
-})
-
-app.get('/expenditure',checkAuthenticated, (req,res) => {
-  res.render('expenditure.ejs', { id: req.user._id, name: req.user.name, admin: req.user.admin, apartment: req.user.apartment, doorNumber: req.user.doorNumber })
+  res.render('water_reading.ejs',{ 
+    name: req.user.name, 
+    secret: process.env.API_SECRET, 
+    admin: req.user.admin, 
+    apartment: req.user.apartment,
+    doorNumber: req.user.doorNumber,
+    contact: req.user.contact
+   })
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
@@ -73,10 +91,20 @@ app.get('/login', checkNotAuthenticated, (req, res) => {
 })
 
 app.get('/register', checkAuthenticated, (req, res) => {
-  res.render('register.ejs', { name: req.user.name, admin: req.user.admin, apartment: req.user.apartment })
+  res.render('register.ejs', { 
+    name: req.user.name, 
+    admin: req.user.admin, 
+    apartment: req.user.apartment,
+    doorNumber: req.user.doorNumber,
+    contact: req.user.contact
+  })
 })
 
-app.get('/users', checkAuthenticated, async (req, res) => {
+app.get('/users', async (req, res) => {
+  if(req.header('Authorization') !== process.env.API_SECRET) {
+    res.status(400).send("Unauthorized");
+    return;
+  }
   const db = await mongodb.MongoClient.connect(process.env.MONGODB_HOST).catch(err => {
     res.status(400).send("Error fetching residents");
   });
@@ -86,7 +114,8 @@ app.get('/users', checkAuthenticated, async (req, res) => {
       for(var i = 0; i < residents.length; i++) {
         var user = {
           id: residents[i]._id,
-          name: residents[i].name
+          name: residents[i].name,
+          contact: residents[i].contact
         }
         users.push(user)
       }
@@ -98,7 +127,11 @@ app.get('/users', checkAuthenticated, async (req, res) => {
     }
 })
 
-app.get('/expenses', checkAuthenticated, async (req, res) => {
+app.get('/expenses', async (req, res) => {
+  if(req.header('Authorization') !== process.env.API_SECRET) {
+    res.status(400).send("Unauthorized");
+    return;
+  }
   const db = await mongodb.MongoClient.connect(process.env.MONGODB_HOST).catch(err => {
     res.status(400).send("Error fetching expenses");
   });
@@ -110,11 +143,13 @@ app.get('/expenses', checkAuthenticated, async (req, res) => {
           type: expenses[i].expense_type,
           date: expenses[i].date_paid,
           amount: expenses[i].amount_paid,
+          paidByID: expenses[i].paid_by_ID,
           paidBy: expenses[i].paid_by,
           description: expenses[i].description,
           individualAmt: expenses[i].individual_amount,
           waterQty: expenses[i].water_quantity,
-          readings: expenses[i].readings
+          readings: expenses[i].readings,
+          contact: expenses[i].contact
         }
         responseList.push(expense)
       }
@@ -126,7 +161,11 @@ app.get('/expenses', checkAuthenticated, async (req, res) => {
     }
 })
 
-app.get('/bookings', checkAuthenticated, async (req, res) => {
+app.get('/bookings', async (req, res) => {
+  if(req.header('Authorization') !== process.env.API_SECRET) {
+    res.status(400).send("Unauthorized");
+    return;
+  }
   const db = await mongodb.MongoClient.connect(process.env.MONGODB_HOST).catch(err => {
     res.status(400).send("Error fetching water bookings");
   });
@@ -139,7 +178,8 @@ app.get('/bookings', checkAuthenticated, async (req, res) => {
           date: bookings[i].date_paid,
           paidBy: bookings[i].paid_by,
           waterQty: bookings[i].water_quantity,
-          totalAmt: bookings[i].amount_paid
+          totalAmt: bookings[i].amount_paid,
+          contact: bookings[i].contact
         }
         responseList.push(booking)
       }
@@ -168,6 +208,7 @@ app.post('/register', checkAuthenticated, async (req, res) => {
       name: req.body.name,
       apartment: req.user.apartment,
       doorNumber: req.body.doorNumber,
+      contact: req.body.contact,
       admin: false
     }
 
@@ -191,7 +232,7 @@ app.post('/register', checkAuthenticated, async (req, res) => {
   }
 })
 
-app.post('/su/register_admin', checkNotAuthenticated, async (req, res) => {
+app.post('/su/register_admin', async (req, res) => {
   try {
     if(req.header('Authorization') !== process.env.API_SECRET) {
       res.status(400).send("Unauthorized");
@@ -206,6 +247,7 @@ app.post('/su/register_admin', checkNotAuthenticated, async (req, res) => {
       name: req.body.name,
       apartment: req.body.apartment,
       doorNumber: req.body.doorNumber,
+      contact: req.body.contact,
       admin: true
     }
 
@@ -233,7 +275,8 @@ app.post('/new_expense',checkAuthenticated, (req,res) => {
       date_paid: req.body.date_paid,
       amount_paid: req.body.amount_paid,
       apartment: req.user.apartment,
-      description: req.body.desc
+      description: req.body.desc,
+      contact: req.body.contact
     }
 
     if(req.body.expense_type != 'corpusFunds') {
@@ -281,10 +324,12 @@ app.post('/water_reading',checkAuthenticated, (req,res) => {
     waterQty = booking.waterQty;
     residents = JSON.parse(req.body.residents);
     readings = {};
+    totalReadings=0;
     for (var i = 0; i < residents.length; i++) {
       residentId = residents[i].id;
       residentName = residents[i].name;
       readingAmt = req.body[residentId];
+      totalReadings += parseInt(readingAmt); 
       amount_payable = (readingAmt/waterQty) * totalAmt;
       reading = {
         "name": residentName,
@@ -293,22 +338,26 @@ app.post('/water_reading',checkAuthenticated, (req,res) => {
       }
       readings[residentId] = reading;
     }
-
-    mongodb.MongoClient.connect(process.env.MONGODB_HOST, function (err, db) {
-      if (err) throw err;
-      var dbo = db.db("amplify");
-      var query = { "_id": new mongodb.ObjectId(bookingId) };
-      var updated = {$set: {readings: readings}};
-      dbo.collection("expenses").updateOne(query, updated, function (err, result) {
-        if (err) {
-          req.flash('info','water readings not added')
-          res.redirect('/water_reading')          
-        } else {
-          req.flash('info','water readings added successfully')
-          res.redirect('/water_reading')          
-        }
+    if(totalReadings>waterQty){
+      req.flash('info','Invalid water readings')
+      res.redirect('/water_reading')
+    } else {
+      mongodb.MongoClient.connect(process.env.MONGODB_HOST, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("amplify");
+        var query = { "_id": new mongodb.ObjectId(bookingId) };
+        var updated = {$set: {readings: readings}};
+        dbo.collection("expenses").updateOne(query, updated, function (err, result) {
+          if (err) {
+            req.flash('info','water readings not added')
+            res.redirect('/water_reading')          
+          } else {
+            req.flash('info','water readings added successfully')
+            res.redirect('/water_reading')          
+          }
+        });
       });
-    });
+    }
   } catch(err) {    
     req.flash('info','error, please try again')
     res.redirect('/water_reading')
